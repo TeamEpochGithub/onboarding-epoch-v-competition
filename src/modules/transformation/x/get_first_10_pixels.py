4ce719a75c12d863146f8540855d260f538b2de0
tolgakopar <cahittolgakopar@gmail.com>
2024-05-25 12:14:03 +0200

    first submission

--- src/modules/transformation/x/get_first_10_pixels.py
+++ src/modules/transformation/x/get_first_10_pixels.py
@@ -14,14 +14,14 @@ from src.modules.transformation.verbose_transformation_block import VerboseTrans
 
 
 @dataclass
-class GetImages(VerboseTransformationBlock):
+class GetFirst10Pixels(VerboseTransformationBlock):
     """Retrieve the images form an XData object."""
 
-    def custom_transform(self, X: XData, **transform_args: Never) -> Sequence[npt.NDArray[np.floating[Any]]] | npt.NDArray[np.floating[Any]]:
+    def custom_transform(self, images: list[npt.NDArray], **transform_args: Never) -> Sequence[npt.NDArray[np.floating[Any]]] | npt.NDArray[np.floating[Any]]:
         """Retrieve the images form an XData object.
 
         :param X: The XData object to get the images from
         :param transform_args: [UNUSED] Any additional arguments
         :return: The images of shape (N, Height, Width, Channels)
         """
-        return X.images
+        return torch.tensor([image.reshape((-1,))[:10] for image in images])
